0555ee7b6e774f19891f8ff249d774af0ac94c8b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-14 22:27:23 +0700

    Datetime | Codelab CurrentTimeMillis

--- src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
+++ src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
@@ -1,9 +1,16 @@
 package com.dicoding.javafundamental.datetime;
 
+
+import java.util.Date;
+
 public class ExampleSystemCurrentMilis {
     public static void main(String[] args) {
         long timeNow = System.currentTimeMillis();
-
         System.out.println("Waktu sekarang adalah " + timeNow + " milisecond");
+
+        // Date
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah " + date.toString());
+
     }
 }
